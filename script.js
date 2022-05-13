e6836c488649355906bf1dbe5e8df1b0cb799596
ashwingudhayakumar <88505120+ashwingudhayakumar@users.noreply.github.com>
2022-05-13 12:12:54 +0530

    audio element source as streamnode.stream

--- script.js
+++ script.js
@@ -773,144 +773,8 @@ newAudioSlider.input.addEventListener("change", function (event) {
 
                 downloadAudio.addEventListener('click', function () {
                     console.log(document.getElementById('speed').value);
-                    function audioBufferToWav (buffer, opt) {
-                        opt = opt || {}
-                      
-                        var numChannels = buffer.numberOfChannels
-                        var sampleRate = buffer.sampleRate
-                        var format = opt.float32 ? 3 : 1
-                        var bitDepth = format === 3 ? 32 : 16
-                      
-                        var result
-                        if (numChannels === 2) {
-                          result = interleave(buffer.getChannelData(0), buffer.getChannelData(1))
-                        } else {
-                          result = buffer.getChannelData(0)
-                        }
-                      
-                        return encodeWAV(result, format, sampleRate, numChannels, bitDepth)
-                      }
-                      
-                      function encodeWAV (samples, format, sampleRate, numChannels, bitDepth) {
-                        var bytesPerSample = bitDepth / 8
-                        var blockAlign = numChannels * bytesPerSample
-                      
-                        var buffer = new ArrayBuffer(44 + samples.length * bytesPerSample)
-                        var view = new DataView(buffer)
-                      
-                        /* RIFF identifier */
-                        writeString(view, 0, 'RIFF')
-                        /* RIFF chunk length */
-                        view.setUint32(4, 36 + samples.length * bytesPerSample, true)
-                        /* RIFF type */
-                        writeString(view, 8, 'WAVE')
-                        /* format chunk identifier */
-                        writeString(view, 12, 'fmt ')
-                        /* format chunk length */
-                        view.setUint32(16, 16, true)
-                        /* sample format (raw) */
-                        view.setUint16(20, format, true)
-                        /* channel count */
-                        view.setUint16(22, numChannels, true)
-                        /* sample rate */
-                        view.setUint32(24, sampleRate, true)
-                        /* byte rate (sample rate * block align) */
-                        view.setUint32(28, sampleRate * blockAlign, true)
-                        /* block align (channel count * bytes per sample) */
-                        view.setUint16(32, blockAlign, true)
-                        /* bits per sample */
-                        view.setUint16(34, bitDepth, true)
-                        /* data chunk identifier */
-                        writeString(view, 36, 'data')
-                        /* data chunk length */
-                        view.setUint32(40, samples.length * bytesPerSample, true)
-                        if (format === 1) { // Raw PCM
-                          floatTo16BitPCM(view, 44, samples)
-                        } else {
-                          writeFloat32(view, 44, samples)
-                        }
-                      
-                        return buffer
-                      }
-                      
-                      function interleave (inputL, inputR) {
-                        var length = inputL.length + inputR.length
-                        var result = new Float32Array(length)
-                      
-                        var index = 0
-                        var inputIndex = 0
-                      
-                        while (index < length) {
-                          result[index++] = inputL[inputIndex]
-                          result[index++] = inputR[inputIndex]
-                          inputIndex++
-                        }
-                        return result
-                      }
-                      
-                      function writeFloat32 (output, offset, input) {
-                        for (var i = 0; i < input.length; i++, offset += 4) {
-                          output.setFloat32(offset, input[i], true)
-                        }
-                      }
-                      
-                      function floatTo16BitPCM (output, offset, input) {
-                        for (var i = 0; i < input.length; i++, offset += 2) {
-                          var s = Math.max(-1, Math.min(1, input[i]))
-                          output.setInt16(offset, s < 0 ? s * 0x8000 : s * 0x7FFF, true)
-                        }
-                      }
-                      
-                      function writeString (view, offset, string) {
-                        for (var i = 0; i < string.length; i++) {
-                          view.setUint8(offset + i, string.charCodeAt(i))
-                        }
-                      }
                     
-
-
-
-                      
-
-                    
-                       var audioCtx=new AudioContext();
-                    //   var    source = audioCtx.createBufferSource();
-                    //   function getData() {
-                          
-                    //       request = new XMLHttpRequest();
-                        
-                    //       request.open('GET', './BlindingLights.mp3', true);
-                        
-                    //       request.responseType = 'arraybuffer';
-                        
-                    //       request.onload = function() {
-                    //         var audioData = request.response;
-                        
-                    //         audioCtx.decodeAudioData(audioData, function(buffer) {
-                    //             myBuffer = buffer;
-                    //             source.buffer = myBuffer;
-                    //             source.playbackRate.value = document.getElementById('speed').value;
-                    //             source.connect(audioCtx.destination);
-                    //             source.loop = true;
-                    //           },
-                        
-                    //           function(e){"Error with decoding audio data" + e.err});
-                        
-                    //       }
-                        
-                    //       request.send();
-                    //     }
-                        
-                      
-                        
-                    //       getData();
-                        
-                       
-                        
-                    
-
-
-
+                    var audioCtx=new AudioContext();
                     
                     let audio;
                     
@@ -923,41 +787,47 @@ newAudioSlider.input.addEventListener("change", function (event) {
                     .then(decodedAudio=>audio=decodedAudio);
                         const playSound=audioCtx.createBufferSource();
                         playSound.buffer=audio;
-                        // playSound.playbackRate.value=document.getElementById('speed').value;
-                        // console.log("playSound.playbackRate.value",playSound.playbackRate.value);
-                        // var sourceAudio=document.getElementById('sourceAudio');
-
+                        playSound.playbackRate.value=document.getElementById('speed').value;
                         const gainNode=audioCtx.createGain();
-                        gainNode.gain.value=0.1;
+                        gainNode.gain.value=document.getElementById('volume').value;
                         playSound.connect(gainNode);
-                        playSound.playbackRate.value=1.75;
-                        gainNode.connect(audioCtx.destination);
-                        playSound.start();
+                        playSound.start(0);
+                        const streamNode = audioCtx.createMediaStreamDestination();
+                        playSound.connect(streamNode);
+                        const audioElem = new Audio();
+                        audioElem.controls = true;
+                        document.body.appendChild(audioElem);
+                        audioElem.srcObject = streamNode.stream;
 
-                        var wav=audioBufferToWav(playSound.buffer);
+
+                        audioElem.addEventListener ('loadeddata', function () {
+        
+                            if (typeof (this.buffered) !== 'undefined' && this.buffered.length > 0) {
+                              
+                              audioElem.addEventListener ('progress', function () {
+                                
+                                var z = (this.buffered.end (0) / this.duration) * 100;
+                                progress.innerText = z;
+                                
+                              });
+                              
+                            }
+                            
+                          });
+                        
                     
                     
                         var anchor = document.createElement('a')
                         document.body.appendChild(anchor)
-                        anchor.style = 'display: none'
-                        var blob = new window.Blob([ new DataView(wav) ], {
-                            type: 'audio/wav'
-                          })
-                          var newaudio=document.createElement('audio');
-                          newaudio.setAttribute('controls','true');
-                          newaudio.src=window.URL.createObjectURL(blob);
-                          document.body.append(newaudio)
+                        anchor.style = 'display: none';
                       
-                          var url = window.URL.createObjectURL(blob)
-                          anchor.href = url
-                          anchor.download = 'newaudio.wav'
+                          anchor.href = streamNode.stream;
+                          anchor.download = 'newaudio.mp3'
                           anchor.click()
-                          window.URL.revokeObjectURL(url)
+                          anchor.remove();
                      
                     
-                        console.log(wav);
-                        //playSound.connect(ctx.destination);
-                        //playSound.start();
+                        
                     }
                     
                     playBack()
@@ -1303,3 +1173,166 @@ window.addEventListener('mouseup', () => {
 
 }
 )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function audioBufferToWav (buffer, opt) {
+//     opt = opt || {}
+  
+//     var numChannels = buffer.numberOfChannels
+//     var sampleRate = buffer.sampleRate
+//     var format = opt.float32 ? 3 : 1
+//     var bitDepth = format === 3 ? 32 : 16
+  
+//     var result
+//     if (numChannels === 2) {
+//       result = interleave(buffer.getChannelData(0), buffer.getChannelData(1))
+//     } else {
+//       result = buffer.getChannelData(0)
+//     }
+  
+//     return encodeWAV(result, format, sampleRate, numChannels, bitDepth)
+//   }
+  
+//   function encodeWAV (samples, format, sampleRate, numChannels, bitDepth) {
+//     var bytesPerSample = bitDepth / 8
+//     var blockAlign = numChannels * bytesPerSample
+  
+//     var buffer = new ArrayBuffer(44 + samples.length * bytesPerSample)
+//     var view = new DataView(buffer)
+  
+//     /* RIFF identifier */
+//     writeString(view, 0, 'RIFF')
+//     /* RIFF chunk length */
+//     view.setUint32(4, 36 + samples.length * bytesPerSample, true)
+//     /* RIFF type */
+//     writeString(view, 8, 'WAVE')
+//     /* format chunk identifier */
+//     writeString(view, 12, 'fmt ')
+//     /* format chunk length */
+//     view.setUint32(16, 16, true)
+//     /* sample format (raw) */
+//     view.setUint16(20, format, true)
+//     /* channel count */
+//     view.setUint16(22, numChannels, true)
+//     /* sample rate */
+//     view.setUint32(24, sampleRate, true)
+//     /* byte rate (sample rate * block align) */
+//     view.setUint32(28, sampleRate * blockAlign, true)
+//     /* block align (channel count * bytes per sample) */
+//     view.setUint16(32, blockAlign, true)
+//     /* bits per sample */
+//     view.setUint16(34, bitDepth, true)
+//     /* data chunk identifier */
+//     writeString(view, 36, 'data')
+//     /* data chunk length */
+//     view.setUint32(40, samples.length * bytesPerSample, true)
+//     if (format === 1) { // Raw PCM
+//       floatTo16BitPCM(view, 44, samples)
+//     } else {
+//       writeFloat32(view, 44, samples)
+//     }
+  
+//     return buffer
+//   }
+  
+//   function interleave (inputL, inputR) {
+//     var length = inputL.length + inputR.length
+//     var result = new Float32Array(length)
+  
+//     var index = 0
+//     var inputIndex = 0
+  
+//     while (index < length) {
+//       result[index++] = inputL[inputIndex]
+//       result[index++] = inputR[inputIndex]
+//       inputIndex++
+//     }
+//     return result
+//   }
+  
+//   function writeFloat32 (output, offset, input) {
+//     for (var i = 0; i < input.length; i++, offset += 4) {
+//       output.setFloat32(offset, input[i], true)
+//     }
+//   }
+  
+//   function floatTo16BitPCM (output, offset, input) {
+//     for (var i = 0; i < input.length; i++, offset += 2) {
+//       var s = Math.max(-1, Math.min(1, input[i]))
+//       output.setInt16(offset, s < 0 ? s * 0x8000 : s * 0x7FFF, true)
+//     }
+//   }
+  
+//   function writeString (view, offset, string) {
+//     for (var i = 0; i < string.length; i++) {
+//       view.setUint8(offset + i, string.charCodeAt(i))
+//     }
+//   }
+
+
+
+
+
+
+
+
+
+
+
+
+//   var    source = audioCtx.createBufferSource();
+                    //   function getData() {
+                          
+                    //       request = new XMLHttpRequest();
+                        
+                    //       request.open('GET', './BlindingLights.mp3', true);
+                        
+                    //       request.responseType = 'arraybuffer';
+                        
+                    //       request.onload = function() {
+                    //         var audioData = request.response;
+                        
+                    //         audioCtx.decodeAudioData(audioData, function(buffer) {
+                    //             myBuffer = buffer;
+                    //             source.buffer = myBuffer;
+                    //             source.playbackRate.value = document.getElementById('speed').value;
+                    //             source.connect(audioCtx.destination);
+                    //             source.loop = true;
+                    //           },
+                        
+                    //           function(e){"Error with decoding audio data" + e.err});
+                        
+                    //       }
+                        
+                    //       request.send();
+                    //     }
+                        
+                      
+                        
+                    //       getData();
